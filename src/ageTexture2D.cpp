dc9d7f9d50e95981b01c5316509c792b6361ef32
Roland <aneragrande@yahoo.com>
2024-01-22 21:45:00 +0300

    Default display method of gl_Texture changed

--- src/ageTexture2D.cpp
+++ src/ageTexture2D.cpp
@@ -28,8 +28,8 @@ int Texture2D::load_from_file(const char* file_name)
 	glBindTexture(GL_TEXTURE_2D, texture_id);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height,
 		0, tex_channels == 4 ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, img);
